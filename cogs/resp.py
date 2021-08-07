75a1ff876e8785e793cbe5883c3f816ae8ea8fc0
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-08-07 14:16:14 +0530

    Update resp.py

--- cogs/resp.py
+++ cogs/resp.py
@@ -1,6 +1,16 @@
 import discord
 import funresponses
 from discord.ext import commands
+import random
+
+simp_gifs = [
+    "https://i.imgur.com/RlrlmmP.gif",
+    "https://c.tenor.com/nOARJZENR9UAAAAS/anime-in-love.gif",
+    "https://c.tenor.com/hCqcNUuWCf0AAAAS/blush-anime.gif",
+    "https://c.tenor.com/u1H1QTx8sxIAAAAC/anime-in-love.gif",
+    "https://c.tenor.com/RzmW-BtosV4AAAAS/show-by-rock-cyan-hijirikawa.gif",
+    "https://c.tenor.com/xc7x66o3rPUAAAAS/anime-love-anime.gif"
+]
 
 class Response(commands.Cog):
     def __init__(self, client):
@@ -10,9 +20,18 @@ class Response(commands.Cog):
     async def simp(self, ctx, mem: discord.Member = None):
         if mem == None:
             mem = ctx.author
-        emb = discord.Embed(titile="", description=f"{ctx.author.mention} simps on {mem.mention} <3\n*`{funresponses.pickup()}`*", color=0x2e69f2)
-        emb.set_image(url="https://i.imgur.com/RlrlmmP.gif")
+        emb = discord.Embed(titile="", description=f"{ctx.author.mention} simps on {mem.mention} <3\n\n*`{funresponses.pickup()}`*", color=0x2e69f2)
+        emb.set_image(url=f"{random.choice(simp_gifs)}")
         await ctx.send(embed=emb)
+    
+    @commands.command()
+    async def roast(self, ctx, mem: discord.Member = None):
+        if mem == None:
+            mem = ctx.author
+        emb = discord.Embed(titile="", description=f"{ctx.author.mention} roasts {mem.mention}..that hurt ouch\n\n*`{funresponses.roast()}`*", color=0x2e69f2)
+        emb.set_image(url="http://pa1.narvii.com/5829/7c80049211a976e27e51ed7ea406c401904289e5_hq.gif")
+        await ctx.send(embed=emb)
+    
         
 def setup(client):
   client.add_cog(Response(client))
