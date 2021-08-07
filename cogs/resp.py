60b03d72c7c4944544a32edb9fca7c98f5f1768f
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-08-07 13:57:49 +0530

    response commands

---
+++ cogs/resp.py
@@ -0,0 +1,17 @@
+import discord
+import funresponses
+from discord.ext import commands
+
+class Response(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    async def simp(self, ctx, mem: discord.Member = None):
+        if mem == None:
+            mem = ctx.author
+        emb = discord.embed(titile="", description=f"{ctx.author.mention} simps on {mem.mention} <3\n*`{funresponses.pickup()}`*", color=0x2e69f2)
+        emb.set_image(url="https://i.imgur.com/RlrlmmP.gif")
+        await ctx.send(embed=emb)
+        
+    
