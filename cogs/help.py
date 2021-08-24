39a1a67ee4cd1a10a11e65751760b797ed9c527a
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-08-24 09:25:48 +0530

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -156,8 +156,8 @@ class Help(commands.Cog):
                         def check(interaction):
                             return interaction.user==ctx.author and interaction.channel == ctx.channel
                         interaction = await self.client.wait_for("select_option",check=check, timeout=40)
-                        print(interaction.component.placeholder)
-                        response = interaction.component.placeholder
+                        print(interaction.component.option.label)
+                        response = interaction.component.option.label
                         if response.lower() == "general":
                             await interaction.respond(type=7, embed=genemb, components=[
                             Select(placeholder="Select", options=[
