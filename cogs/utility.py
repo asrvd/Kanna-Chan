9b07d7bf7b0f92a3d7ed4054c3f5f31c12539428
ashish <68690233+awsheeshh@users.noreply.github.com>
2021-10-09 10:15:25 +0530

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -13,6 +13,9 @@ db = firebase.database()
 
 def create(guild, channel):    #stores guild ID and channel ID
     db.child("WELCOME").child(guild).set({"CHANNEL": channel})
+    
+def remove(guild):
+    db.child("WELCOME").child(guild).remove()
 
 def return_channel(guild):     #returns channel ID
     channel = db.child("WELCOME").child(guild).child("CHANNEL").get().val()
@@ -31,6 +34,16 @@ class Utility(commands.Cog):
     async def wsetup(self, ctx, *, channel: discord.TextChannel = None):
         create(ctx.guild.id, channel.id)
         await ctx.send(f">> Welcome messages have been setup to {channel.mention} now.")
+        
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def wdisable(self, ctx):
+        if return_channel(ctx.guild.id) == None:
+            await ctx.reply("Welcome message has not been setup for this server.")
+        else:
+            remove(ctx.guild.id)
+            await ctx.reply("Successfully removed welcome message setup for this server.")
+        
     
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -39,31 +52,38 @@ class Utility(commands.Cog):
         if channel_id == None:
             return
         elif channel_id != None:
-            if member.bot:
-                return
+            if member.guild.id == 864220272444571658:
+                if member.bot:
+                    return
+                else:
+                    message = f"{member.mention} just joined the server! Everyone welcome our newest member! <a:explosion_heart:877426228775227392>"
+                    await channel.send(message)
             else:
-                guild_name = member.guild.name
-                name = member.name
-                id = member.discriminator
-                f1 = ImageFont.truetype('./fonts/Magz.otf', 128)
-                f2 = ImageFont.truetype('./fonts/Magz.otf', 64)
-                desc = f"Welcome {member.mention} to the Server **{guild_name}**!"
-                bg = Image.new('RGBA', (1000, 200), (255, 0, 0, 0))
-                asset = member.avatar_url_as(format='png', size=256)
-                await asset.save('av.png')
-                im = Image.open('av.png')
-                im = im.resize((200, 200))
-                mask = Image.new('L', (200, 200), 0)
-                mask_draw = ImageDraw.Draw(mask)
-                mask_draw.ellipse((0, 0, 200, 200), fill=255)
-                mask.save('mask.jpg')
-                bg.paste(im, (30, 0), mask)
-                draw = ImageDraw.Draw(bg)
-                draw.text((300, 20), name, (144, 66, 245), font=f1)
-                draw.text((300, 119), id, (247, 87, 237), font=f2)
-                bg.save('final.png')
-                file = discord.File('final.png')
-                await channel.send(desc, file=file)
+                if member.bot:
+                    return
+                else:
+                    guild_name = member.guild.name
+                    name = member.name
+                    id = member.discriminator
+                    f1 = ImageFont.truetype('./fonts/Magz.otf', 128)
+                    f2 = ImageFont.truetype('./fonts/Magz.otf', 64)
+                    desc = f"Welcome {member.mention} to the Server **{guild_name}**!"
+                    bg = Image.new('RGBA', (1000, 200), (255, 0, 0, 0))
+                    asset = member.avatar_url_as(format='png', size=256)
+                    await asset.save('av.png')
+                    im = Image.open('av.png')
+                    im = im.resize((200, 200))
+                    mask = Image.new('L', (200, 200), 0)
+                    mask_draw = ImageDraw.Draw(mask)
+                    mask_draw.ellipse((0, 0, 200, 200), fill=255)
+                    mask.save('mask.jpg')
+                    bg.paste(im, (30, 0), mask)
+                    draw = ImageDraw.Draw(bg)
+                    draw.text((300, 20), name, (144, 66, 245), font=f1)
+                    draw.text((300, 119), id, (247, 87, 237), font=f2)
+                    bg.save('final.png')
+                    file = discord.File('final.png')
+                    await channel.send(desc, file=file)
 
 
     @commands.command()
